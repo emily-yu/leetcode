ad68353ba9fd833dd40a410b8ce3e974975949d7
Emily Yu <eyudeveloper@gmail.com>
2020-10-14 14:24:21 -0700

    q34 binarysearch.io

--- 34.find-first-and-last-position-of-element-in-sorted-array.py
+++ 34.find-first-and-last-position-of-element-in-sorted-array.py
@@ -9,6 +9,7 @@ class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
         
         # brute force o(n)
+        '''
         start = -1
         end = -1
         ind = 0
@@ -24,6 +25,40 @@ class Solution:
             end = start # only one occurance
 
         return (start, end)
-                
+        '''
+
+        def binarySearchHelper(elem, arr, start, end):
+            if start > end:
+                return -1
+            mid = (start + end)//2
+            if arr[mid] == elem:
+                return mid
+            elif arr[mid] > elem:
+                # recurse to the left of mid
+                return binarySearchHelper(elem, arr, start, mid - 1)
+            else:
+                # recurse to the right of mid
+                return binarySearchHelper(elem, arr, mid + 1, end)
+
+    
+        mdpt = len(nums) // 2
+        print("search ", nums, mdpt)
+
+        ind = binarySearchHelper(target, nums, 0, len(nums) - 1)
+        print(ind)
+        right = ind
+
+        if ind is not None:
+            while ind > 0 and nums[ind - 1] == target:
+                print(nums[ind])
+                ind -= 1
+            
+            while right < len(nums) - 1 and nums[right + 1] == target:
+                print(nums[ind])
+                right += 1
+            return (ind, right)
+        
+        # if ind is not found
+        return (-1, -1)
 # @lc code=end
 
