445464cfbcacf77eecf9142cc0e9a168cdd5676f
Emily Yu <eyudeveloper@gmail.com>
2020-12-13 15:16:55 -0800

    q102 queue implementation

--- 102.binary-tree-level-order-traversal.py
+++ 102.binary-tree-level-order-traversal.py
@@ -51,5 +51,62 @@ class Solution:
                 result.append(tracking[i])
 
         return result
+
+        # q = []
+        # if root is None:
+        #     return q
+
+        # q.append(root) # base
+        # if root.left is None and root.right is None:
+        #     return [[q[0].val]]
+            
+        # complete = []
+
+        # while len(q) > 0:
+        #     curr = q.pop(0)
+
+        #     if curr is not None:
+        #         q += [curr.left]
+        #         q += [curr.right]
+        #         complete.append(curr)
+        #     else:
+        #         complete.append(-999)
+        
+        # for elem in complete:
+        #     if type(elem) == int:
+        #         print(elem)
+        #     else:
+        #         print(elem.val)
+        # # [root.left, root.right] [root]
+        # # [root.left.left, root.left.right] [root.left, root.right, root]
+
+        # result = [] # return array
+        # level = 0 #2^0 = 1 node, 2^1 = 2 nodes
+        # level_count = 2**level
+        # start = 0
+        # node_left = len(complete) # 5 -> 4 -> 2 -> look for 4, only 2 left; node(left) < level_count
+        # for node in complete:
+        #     level_result = []
+        #     print("compare: ", level_count, node_left)
+        #     # if level_count < node_left: # full amount of nodes
+        #     print(complete[start:start+level_count])
+        #     for elem in complete[start:start+level_count]:
+        #         if elem != -999:
+        #             level_result.append(elem.val)
+        #         else:
+        #             level_result.append(-999)
+        #     start += level_count
+        #     level += 1
+        #     level_count = 2**(level)
+        #     print(level_result)
+        #     if level_result != []:
+        #         result.append(level_result)
+        # return result
+        
+
+
+        
+
+
 # @lc code=end
 
