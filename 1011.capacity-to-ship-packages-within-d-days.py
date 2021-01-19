ad21254d0ba4671c565e21c40af777f768f73736
Emily Yu <eyudeveloper@gmail.com>
2021-01-18 22:58:16 -0800

    q1011 time exceed bfs

--- 1011.capacity-to-ship-packages-within-d-days.py
+++ 1011.capacity-to-ship-packages-within-d-days.py
@@ -7,6 +7,57 @@
 # @lc code=start
 class Solution:
     def shipWithinDays(self, weights: List[int], D: int) -> int:
+        # for optimization
+        memo = dict()
+
+        # test case
+        # weights = [1, 2, 3, 1, 1]
+        # D = 3
+
+        # weights = [3, 2, 2, 4, 1, 4]
+        # D = 3
+
+        result = []
+
+        # init bfs
+        q = []
+        q.append(([], weights, 0))
+
+        # create all combinations possible bfs
+        while len(q) > 0:
+            root = q.pop(0) # [result, remaining, level]
+            if root[2] == D: # done, reached level
+                if root[1] == []: # touched all
+                    result.append(root[0] + [root[1][0:i+1]])
+                    # print('appending: ', root)
+                else:
+                    pass
+            else: # generate the rest of the combinations
+                for i in range(len(root[1])):
+                    # print(root[0], root[1][0:i+1])
+                    # print('level', root[2] + 1)
+
+                    # amount of children validation
+                    if root[1][i+1:] == [] and root[2] != D-1:
+                        # print('omit')
+                        pass
+                    else: # enough children for next level
+                        # print(root[0] + [root[1][0:i+1]])
+                        q.append((root[0] + [root[1][0:i+1]], root[1][i+1:], root[2] + 1))
+                        # break
+            # print(q)
+        # print(result)
+
+        # pick out smallest value
+        minval = float('inf')
+        for item in result:
+            # print(item)
+            sections = [sum(part) for part in item]
+            # print(max(sections))
+            if max(sections) < minval:
+                minval = max(sections)
+
+        return minval
         '''
         [3, 2, 2, 4, 1, 4]
         days = 4
