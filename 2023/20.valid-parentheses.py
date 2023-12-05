04ba2f29567da202c0de4417199b17a9f1147f9b
emily-yu <eyudeveloper@gmail.com>
2023-12-05 11:28:40 -0800

    #20 EASY (proper)

--- 2023/20.valid-parentheses.py
+++ 2023/20.valid-parentheses.py
@@ -14,7 +14,6 @@ class Solution(object):
 
         # edge cases
         l = len(s) 
-        print(l)
         if l == 0: #empty (valid) 
             return True
         if l == 1: #too short (invalid)
@@ -23,44 +22,38 @@ class Solution(object):
             return False
         
         #space
-        left_keys = ["[", "{", "("]
-        left = {
-            '[': [],
-            '{': [],
-            '(': []
-        }
+        stk = []
+        i = 0
         match = {
             ']': '[',
             '}': '{',
             ')': '('
         }
-        i = 0
         
         # iterate through
         while i < l: # end after last char
             char = s[i]
-            print(char)
             # if left, add to list
-            if char in left_keys: 
-                left[char].append(i)
+            if char in ['(', '[', '{']: 
+                stk.append(char)
 
-            # if right, check for match
+            # if right, check for possible match
             else:
-                pool = left[match[char]]
-                if len(pool) > 0:
-                    pool.pop()
-                # note: if right and no left then too many right chars
+                # left chars exist?
+                if len(stk) > 0: 
+                    left = stk.pop()
                 else:
+                    return False
+                
+                # match?
+                if match[char] != left:
                     return False
             
             i += 1 # inc
 
-        print(left)
-        # check all empty
-        for ind in left:
-            if len(left[ind]) > 0:
-                return False
-        
+        # if still left chars then doom
+        if len(stk) > 0:
+            return False
         return True
         
         
