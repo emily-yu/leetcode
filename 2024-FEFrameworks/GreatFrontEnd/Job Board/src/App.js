e258bf84f3e2e400a3a785a6bc38d9a01a483099
emily-yu <eyudeveloper@gmail.com>
2024-07-21 12:32:46 -0700

    gfe: job board (async await)

--- 2024-FEFrameworks/GreatFrontEnd/Job Board/src/App.js
+++ 2024-FEFrameworks/GreatFrontEnd/Job Board/src/App.js
@@ -7,62 +7,43 @@ import {useState, useEffect, Component} from 'react'
 export default function App() {
   
   // storing blog post states
-  const [blogPosts, setBlogPosts] = useState();
-
-  // cannot use this, because not a class
-  // componentDidMount() {
-  //   getData()
-  // }
+  const [blogPosts, setBlogPosts] = useState([]);
 
   // on load
-  useEffect(() => {
-    // getData()
-  })
-// {JSON.stringify(blogPosts)}
-  return <div><CardWrapper/></div>;
-}
+  useEffect(() => {    
 
-// using a separate Card component (componentDidMount, componentDidUpdate)
-class CardWrapper extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      blogPosts: []
-    }
-  }
-  componentDidMount() {
-    console.log("mounted")
-    
-    console.log('hello')
-    fetch(`https://hacker-news.firebaseio.com/v0/jobstories.json`)
+    const derp = async() => {
+// nested fetches. https://stackoverflow.com/a/57075730
+    const ids = await fetch(`https://hacker-news.firebaseio.com/v0/jobstories.json`)
         .then(response => response.json())
         .then(data => {
-          console.log("arrivals")
+          // console.log("arrivals")
           console.log(data)
-          this.setState({
-            blogPosts: data
-          })
-        })
-        .catch(err => {
+          return data
+        }).catch(err => {
           console.log(err)
         })
-  }
-  
-  // componentDidUpdate() {
-
-  // }
-
-  render() {
-    return (
-      <>
-        <ul>
-          {
-            // this.state.blogPosts
-            this.state.blogPosts.map((item) => <li>{JSON.stringify(item)}</li>)
+        let result = []
+        for (let item in ids) {
+          const data = await fetch(`https://hacker-news.firebaseio.com/v0/item/${item}.json`)
+            .then(response => response.json())
+            .then(data => {
+              // console.log("it's only adding on each rerender: ", data)
+              return data
+            })
+            result.push(data)
+            // console.log("curr result:", result)
           }
-        </ul>
-        <div>Card: asdf</div>
-      </>
-    )
-  }
+    
+        console.log(ids, result)
+        return result
+      
+    }
+    derp().then((data) => {
+      console.log("the final:", data)
+      setBlogPosts(data)
+    })
+    
+  })
+  return <ul>{ blogPosts && JSON.stringify(blogPosts)}</ul>;
 }
